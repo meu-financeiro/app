518f3141b27f269db06722e8e09090559702539c
Alex Silva <alexro.kcoal@gmail.com>
2025-11-21 01:53:39 -0400

    wip

--- src/main.js
+++ src/main.js
@@ -6,6 +6,15 @@ async function init() {
   await initConfig();
   handleAuthState();
 
+  const statusEl = document.getElementById("status");
+
+  try {
+    const lancamentos = await LancamentoService.list();
+    statusEl.textContent = JSON.stringify(lancamentos, null, 2); // identação de 2 espaços
+  } catch (err) {
+    statusEl.textContent = "Erro ao carregar lançamentos: " + err.message;
+  }
+
   // console.log("Lançamentos carregados:", await LancamentoService.list());
 
   // console.log("Lançamento carregado:", await LancamentoService.get(1));
